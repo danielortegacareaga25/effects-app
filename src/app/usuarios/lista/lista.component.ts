5251e8f692cb05b755dd7b005068d536a75ad386
Daniel Ortega <danielortegacareaga@gmail.com>
2020-09-14 15:13:47 -0500

    Terminado de maquetado

---
+++ src/app/usuarios/lista/lista.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from './../../services/usuario.service';
+import { Usuario } from './../../models/usuario.model';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styles: [],
+})
+export class ListaComponent implements OnInit {
+  usuarios: Usuario[] = [];
+  constructor(private usuarioService: UsuarioService) {}
+
+  ngOnInit(): void {
+    this.usuarioService.getUsers().subscribe((data) => {
+      this.usuarios = data;
+    });
+  }
+}
